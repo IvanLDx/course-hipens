62eeea21ac2784faa45f428c125f6e3f1565d10d
Iván Hermo <ratzi.dx@gmail.com>
2025-10-30 19:53:00 +0100

    HC-005. Building Our Product API - Controllers Part 1

---
+++ backend/controllers/productController.js
@@ -0,0 +1,39 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/projectModel.js';
+
+const getProduct = asyncHandler(async (req, res) => {
+	const pageSize = 8;
+
+	const page = Number(req.query.pageNumber) || 1;
+
+	const keyword = req.query.keyword
+		? {
+				name: {
+					$regex: req.query.keyword,
+					$options: 'i'
+				}
+		  }
+		: {};
+
+	const count = await Product.countDocuments({ ...keyword });
+	const products = await Product.find({ ...keyword })
+		.limit(pageSize)
+		.skip(pageSize * (page - 1));
+
+	res.json({
+		products,
+		page,
+		pages: Math.ceil(count / pageSize),
+		total: count
+	});
+});
+
+const getProductById = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		res.json(product);
+	}
+	res.status(404);
+	throw new Error('Product not found');
+});
