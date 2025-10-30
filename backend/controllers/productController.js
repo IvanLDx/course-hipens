0e0b3fc2ee20780473e784be5cd57550d8ef8d92
Iván Hermo <ratzi.dx@gmail.com>
2025-10-30 20:02:58 +0100

    HC-006. Building Our Product API - Controllers Part 2

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -37,3 +37,52 @@ const getProductById = asyncHandler(async (req, res) => {
 	res.status(404);
 	throw new Error('Product not found');
 });
+
+const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: 'Sample name',
+		price: 0,
+		image: '/uploads/sample.png',
+		category: 'Sample Category',
+		countInStock: 0,
+		numReviews: 0,
+		description: 'Sample description'
+	});
+
+	const createdProduct = await product.save();
+
+	res.status(201).json(createdProduct);
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+	const { name, price, description, image, category, countInStock } = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		product.name = name;
+		product.price = price;
+		product.description = description;
+		product.image = image;
+		product.category = category;
+		product.countInStock = countInStock;
+
+		const updateProduct = await product.save();
+		res.json(updateProduct);
+	} else {
+		res.status(404);
+		throw new Error('Product not found');
+	}
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		await Product.deleteOne({ _id: product._id });
+		res.json({ message: 'Product successfully deleted' });
+	} else {
+		res.status(404);
+		throw new Error('Proudct not found');
+	}
+});
