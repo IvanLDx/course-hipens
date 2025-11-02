770b651cd8503b30dc44955705ed1f8c3900fc90
Iván Hermo <ratzi.dx@gmail.com>
2025-11-02 23:48:33 +0100

    HC-023. Start With Redux Toolkit - Base API Slice

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,9 +1,11 @@
 import { Stack } from 'expo-router';
 
-export default function RootLayout() {
+const RootLayout = () => {
 	return (
 		<Stack screenOptions={{ headerShown: false }}>
 			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 		</Stack>
 	);
-}
+};
+
+export default RootLayout;
