1774c64cc64f321c87958145523367977a6298cd
Iván Hermo <ratzi.dx@gmail.com>
2025-11-03 22:19:06 +0100

    HC-026. Message Component

---
+++ frontend/components/Message.jsx
@@ -0,0 +1,56 @@
+import { StyleSheet, View, Text } from 'react-native';
+import { Colors } from '../constants/Utils';
+
+const Message = ({ variant = 'error', children }) => {
+	const variantStyles = {
+		success: {
+			backgroundColor: Colors.successLight,
+			color: Colors.success,
+			borderColor: Colors.successBorder
+		},
+		error: {
+			backgroundColor: Colors.error,
+			color: Colors.textRed,
+			borderColor: Colors.errorBorder
+		},
+		info: {
+			backgroundColor: Colors.infoLight,
+			color: Colors.info,
+			borderColor: Colors.infoBorder
+		}
+	};
+
+	const selectedStyle = variantStyles[variant] || variantStyles.error;
+
+	return (
+		<View
+			style={[
+				styles.container,
+				{
+					backgroundColor: selectedStyle.backgroundColor,
+					borderLeftColor: selectedStyle.borderColor
+				}
+			]}
+		>
+			<Text style={([styles.test], { color: selectedStyle.color })}>{children}</Text>
+		</View>
+	);
+};
+
+export default Message;
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 12,
+		borderLeftWidth: 4,
+		borderRadius: 4,
+		marginVertical: 8,
+		width: '100%',
+		alignItems: 'center'
+	},
+	text: {
+		fontSize: 14,
+		lineHeight: 20,
+		fontWeight: '500'
+	}
+});
