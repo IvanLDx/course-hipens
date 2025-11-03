c08421ff9fb24bf2aa9498991a8181e94525a130
Iván Hermo <ratzi.dx@gmail.com>
2025-11-03 22:42:58 +0100

    HC-028. Building the Product Component Part 2

--- frontend/components/Product.jsx
+++ frontend/components/Product.jsx
@@ -38,6 +38,13 @@ const Product = ({ product }) => {
 
 					<View style={styles.pricing}>
 						<Text style={styles.currentPrice}>${product.price}</Text>
+						<View style={[styles.availability, styles.countInStock > 0 ? styles.available : styles.unavailable]}>
+							<Text style={styles.availability}>{product.countInStock > 0 ? 'In Stock' : 'Sold out'}</Text>
+						</View>
+					</View>
+
+					<View style={styles.ratingRow}>
+						<Rating value={product} tet={`${product.numReviews} reviews`} />
 					</View>
 				</View>
 			</TouchableOpacity>
@@ -47,4 +54,76 @@ const Product = ({ product }) => {
 
 export default Product;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		width: '45%',
+		backgroundColor: Colors.white,
+		borderRadius: 15,
+		padding: 16,
+		marginVertical: 8,
+		marginHorizontal: '2%',
+		shadowColor: Colors.darkGray,
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.08,
+		shadowRadius: 8,
+		elevation: 2,
+		borderWidth: 1,
+		borderColor: Colors.lightGray
+	},
+	imageWrapper: {
+		backgroundColor: Colors.white,
+		borderRadius: 12,
+		overflow: 'hidden',
+		marginBottom: 8,
+		alignItems: 'center',
+		juntifyContent: 'center',
+		height: 140,
+		padding: 8,
+		borderWidth: 1,
+		borderColor: Colors.lightGray
+	},
+	image: {
+		width: '100%',
+		height: '100%'
+	},
+	infoArea: {
+		paddingHorizontal: 4
+	},
+	productName: {
+		fontSize: 15,
+		fontWeight: '600',
+		color: Colors.textColor,
+		marginBottom: 10,
+		height: 36,
+		lineHeight: 20
+	},
+	pricing: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 10
+	},
+	currentPrice: {
+		fontSize: 14,
+		fontWeight: '700',
+		color: Colors.primary
+	},
+	availability: {
+		padding: 10,
+		borderRadius: 20
+	},
+	availabilityText: {
+		fontSize: 10,
+		fontWeight: '500',
+		color: Colors.white
+	},
+	available: {
+		backgroundColor: Colors.inStock
+	},
+	unavailable: {
+		backgroundColor: Colors.soldOut
+	},
+	ratingRow: {
+		maginTop: 6
+	}
+});
