fd2ec73b62435e9a85d4130093583947b81e7b40
Iván Hermo <ratzi.dx@gmail.com>
2025-11-06 19:37:32 +0100

    HC-050. Implementing the User Login Screen Part 1

--- frontend/app/(tabs)/profile.jsx
+++ frontend/app/(tabs)/profile.jsx
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { useRouter } from 'expo-router';
 
 const profile = () => {
+	const router = useRouter();
+	const handleLoginPress = () => router.push('/loginScreen');
+
 	return (
 		<View>
 			<Text>profile</Text>
+			<Text onPress={handleLoginPress} style={{ margin: 50 }}>
+				Login
+			</Text>
 		</View>
 	);
 };
