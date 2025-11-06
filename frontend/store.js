7a35863dbd8ae7f5809df12c51aa170195168592
Iván Hermo <ratzi.dx@gmail.com>
2025-11-06 19:24:56 +0100

    HC-049. Persisting Our Redux Store - Integrating Redux-Persist for Auth and Cart

--- frontend/store.js
+++ frontend/store.js
@@ -1,14 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
+import authReducer from './slices/authSlice';
+
+const persistConfig = {
+	key: 'root',
+	storage: AsyncStorage,
+	whitelist: ['auth', 'cart']
+};
+
+const rootReducer = combineReducers({
+	[apiSlice.reducerPath]: apiSlice.reducer,
+	auth: authReducer,
+	cart: cartSliceReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-	reducer: {
-		[apiSlice.reducerPath]: apiSlice.reducer,
-		cart: cartSliceReducer
-	},
-	middleware: (getDefaultMidlleware) => getDefaultMidlleware().concat(apiSlice.middleware),
-	devtools: true
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoreActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/REGISTER']
+			}
+		}).concat(apiSlice.middleware),
+	devTools: true
 });
+
+export const persistor = persistStore(store);
 
 export default store;
