71e1b6b18693d3927d88b16e3e55e31bf06a8cd4
IvanLDx <ratzi.dx@gmail.com>
2025-11-23 19:58:34 +0100

    HC-104. Implementing The Product List Screen Part 2

--- frontend/app/(screens)/admin/ProductListScreen.jsx
+++ frontend/app/(screens)/admin/ProductListScreen.jsx
@@ -50,10 +50,90 @@ const ProductListScreen = () => {
 		]);
 	};
 
+	const renderPaginationButtons = () => {
+		if (!data?.pages || data.pages.length <= 1) {
+			return null;
+		}
+
+		return (
+			<View style={styles.paginationContainer}>
+				{Array.from({ length: data.pages }, (_, i) => i + 1).map((page) => (
+					<TouchableOpacity
+						key={page}
+						style={[styles.pageButton, page === data.page && styles.activePageButton]}
+						onPress={() => {
+							router.setParams({
+								pageNumber: page.toString()
+							});
+						}}
+					>
+						<Text style={[styles.pageButtonText, page === data.page && styles.activePageButtonText]}>{page}</Text>
+					</TouchableOpacity>
+				))}
+			</View>
+		);
+	};
+
+	if (error) {
+		return (
+			<View style={styles.container}>
+				<Message variant="error">
+					<Text>{error?.data?.message || error.error}</Text>
+				</Message>
+			</View>
+		);
+	}
+
 	return (
-		<View>
-			<Text>ProductListScreen</Text>
-		</View>
+		<SafeAreaView style={styles.safeArea}>
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<Text style={styles.title}>Products</Text>
+
+					<TouchableOpacity style={styles.addButton} onPress={createProductHandler}>
+						<FontAwesome name="plus" size={16} color={Colors.white} />
+
+						<Text style={styles.addButtonText}>Add a product</Text>
+					</TouchableOpacity>
+				</View>
+
+				<View style={styles.tableHeader}>
+					<Text style={[styles.headerCell, { flex: 2 }]}>Name</Text>
+					<Text style={[styles.headerCell, { flex: 1 }]}>Price</Text>
+					<Text style={[styles.headerCell, { flex: 1.5 }]}>Actions</Text>
+				</View>
+
+				<FlatList
+					data={data?.products}
+					keyExtractor={(item) => item._id}
+					contentContainerStyle={{ paddingBottom: 20 }}
+					renderItem={({ item: product }) => (
+						<View style={styles.tableRow}>
+							<Text style={[styles.cell, { flex: 2 }]} numberOfLines={1}>
+								{product.name}
+							</Text>
+
+							<Text style={[styles.cell, { flex: 1 }]} numberOfLines={1}>
+								${product.price}
+							</Text>
+
+							<View style={[styles.actionsCell, { flex: 1.5 }]}>
+								<TouchableOpacity
+									onPress={() =>
+										router.push({
+											pathname: '/admin/ProductEdotScreen',
+											params: { id: product._id }
+										})
+									}
+								>
+									<FontAwesome name="edit" color={Colors.primary} size={20} />
+								</TouchableOpacity>
+							</View>
+						</View>
+					)}
+				></FlatList>
+			</View>
+		</SafeAreaView>
 	);
 };
 
